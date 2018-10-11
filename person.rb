af2ef971dd16610f41b71e9d9121444e05e2bd59
Annie Eby <eby.annie@gmail.com>
2018-10-11 13:04:17 +0000

    automatically backed up by learn

---
+++ person.rb
@@ -0,0 +1,20 @@
+class Person
+  def
+  initialize(name)
+  @name = name
+  end
+
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+end
+
+kanye = Person.new("Kanye")
+kanye.name
+
+name="Yeezy"
+kanye.name
